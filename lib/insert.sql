05aa5b87b7047d1d370fb4b0b9c1ee44d9159483
Jim Hough <teachlife@outlook.com>
2019-05-28 02:00:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,6 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 'Later Dude', 1, 1), (2, 'See ya man', 2, 2);
 INSERT INTO subgenres (id, name) VALUES (1, 'prefix'), (2, 'foxxy');
 INSERT INTO authors (id, name) VALUES (1, 'Jack Daniels'), (2, 'Johnny Walker')
+INSERT INTO books (id, title, year, series_id) VALUES (1, 'The ideas', 1997, 1), (2, 'The thoughts', 1998, 1), (3, 'The actions', 1999, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (4, 'Wicked', 1990, 2), (5, 'White', 1991, 2), (6, 'Black', 1992, 2);
+INSERT INTO characters ()

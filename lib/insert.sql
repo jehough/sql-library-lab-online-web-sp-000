912bd5b74051ea0be4d0d670c8efb0a8c12b3804
Jim Hough <teachlife@outlook.com>
2019-05-28 01:55:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 'Later Dude', 1, 1), (2, 'See ya man', 2, 2)
-INSERT INTO subgenres (id, name) VALUES (1, 'prefix'), (2, 'foxxy')
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 'Later Dude', 1, 1), (2, 'See ya man', 2, 2);
+INSERT INTO subgenres (id, name) VALUES (1, 'prefix'), (2, 'foxxy');

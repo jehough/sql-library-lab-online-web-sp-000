a3f9d300917660726889c5d0fba3c58bd52d425c
Jim Hough <teachlife@outlook.com>
2019-05-28 02:34:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM characters
   INNER JOIN series ON characters.series_id = series.id
   GROUP BY series.title
-  HAVING characters.species = 'human';"
+  HAVING characters.species = 'human'
+  ORDER COUNT(characters.species);"
 end
 
 def select_character_names_and_number_of_books_they_are_in

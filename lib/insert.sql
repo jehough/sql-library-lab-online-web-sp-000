21d422961df2acd8896d8d49d7966a932aa55b6a
Jim Hough <teachlife@outlook.com>
2019-05-28 02:10:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,5 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (3, "Moe", 'Crack it', 'duck', 1, 1), (4, 'Mack', 'Throw it', 'lion', 1, 1);
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Joe", 'Break it', 'deer', 2, 2), (6, 'Jack', 'Fix it', 'bear', 2, 2);
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (7, "Moe", 'Crack it', 'duck', 2, 2), (8, 'Mack', 'Throw it', 'lion', 2, 2);
+INSERT INTO character_books(id, book_id, character_id) VALUES (1, 1, 1), (2, 2, 1), (3, 3, 1), (4, 1, 2), (5, 2, 2), (6, 3, 2), (7, 2, 3), (8, 3, 4),
+(9, 4, 5), (10, 5, 5), (11, 6, 5), (12, 4, 6), (13, 5, 6), (14, 6, 6), (15, 4, 7), (16, 5, 8);
